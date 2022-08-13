52c41f803254a63a30b733f4df9184f5216c054c
mgoma <mgoma@MOHAMMAD-REDA>
2022-08-13 15:19:52 +0200

    Add project files.

---
+++ Asp_Github/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp_Github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
